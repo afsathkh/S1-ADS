10831e0f549bd00c8a7d0acc6c8b01bf999b8ebc
Afsath K H <afsa.ali735@gmail.com>
2025-12-01 23:34:30 +0530

    first commit

--- queuearray.c
+++ queuearray.c
@@ -1,77 +1,52 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#define N 10
-
-int queue[N];
-int rear = -1, front = -1;
-
-void enqueue() {
-    int val;
-    printf("Enter the data: ");
-    scanf("%d", &val);
-
-    if (rear == N - 1) {
-        printf("Queue is overflow\n");
-        return;
-    }
-    else if (front == -1 && rear == -1) {
-        front = rear = 0;
-        queue[rear] = val;
-    }
-    else {
-        rear++;
-        queue[rear] = val;
-    }
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+int queue[20];
+int front=-1,rear=-1;
+int opt,value,i;
+while(1){
+printf("\n1 ENQUEUE\n2 DEQUEUE\n3 DISPLAY\n4 EXIT\n");
+printf("Choose option:");
+scanf("%d",&opt);
+if(opt==1){
+printf("Enter value:");
+scanf("%d",&value);
+if(front==-1 && rear==-1){
+front=0;
+rear=0;
+queue[rear]=value;
+printf("Inserted %d\n",value);
 }
-
-void dequeue() {
-    if (front == -1 && rear == -1) {
-        printf("Queue is underflow\n");
-        return;
-    }
-    else if (front == rear) {
-        printf("Deleted element is %d\n", queue[front]);
-        front = rear = -1;
-    }
-    else {
-        printf("Deleted element is %d\n", queue[front]);
-        front++;
-    }
+else{
+rear++;
+queue[rear]=value;
+printf("Inserted %d\n",value);
 }
-
-void display() {
-    if (front == -1 && rear == -1) {
-        printf("Queue is empty\n");
-        return;
-    }
-
-    printf("Queue elements: ");
-    for (int i = front; i <= rear; i++) {
-        printf("%d ", queue[i]);
-    }
-    printf("\n");
 }
-
-int main() {
-    int opt;
-
-    while (1) {
-        printf("\n1) ENQUEUE\n");
-        printf("2) DEQUEUE\n");
-        printf("3) DISPLAY\n");
-        printf("4) EXIT\n");
-        printf("Enter your option: ");
-        scanf("%d", &opt);
-
-        switch (opt) {
-            case 1: enqueue(); break;
-            case 2: dequeue(); break;
-            case 3: display(); break;
-            case 4: exit(0);
-            default: printf("Invalid option\n");
-        }
-    }
-
-    return 0;
+else if(opt==2){
+if(front==rear){
+printf("Deleted %d\n",queue[front]);
+front=-1;
+rear=-1;
+}
+else{
+printf("Deleted %d\n",queue[front]);
+front++;
+}
+}
+else if(opt==3){
+printf("Queue elements:");
+for(i=front;i<=rear;i++){
+printf("%d ",queue[i]);
+}
+printf("\n");
+}
+else if(opt==4){
+exit(0);
+}
+else{
+printf("Invalid option\n");
+}
+}
+return 0;
 }
