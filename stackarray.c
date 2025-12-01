d2c7b505baf8c4a5b734a27be05b63550eb4af95
Afsath K H <afsa.ali735@gmail.com>
2025-12-01 23:22:23 +0530

    first commit

--- stackarray.c
+++ stackarray.c
@@ -1,93 +1,33 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-
-#define MAX 100
-
-int stack[MAX];
-int top = -1;
-
-// Check if stack is full
-bool isFull() {
-    return (top == MAX - 1);
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+int stack[100],top=-1,opt,data,i;
+while(1){
+printf("\n1 PUSH\n2 POP\n3 PEEK\n4 DISPLAY\n5 EXIT\n");
+printf("Choose option:");
+scanf("%d",&opt);
+if(opt==1){
+printf("Enter data:");
+scanf("%d",&data);
+top++;
+stack[top]=data;
+printf("Pushed %d",data);
 }
-
-// Check if stack is empty
-bool isEmpty() {
-    return (top == -1);
+else if(opt==2){
+printf("Popped %d\n",stack[top]);
+top--;
 }
-
-// Push operation
-void push() {
-    int data;
-
-    if (isFull()) {
-        printf("Stack is full\n");
-        return;
-    }
-
-    printf("Enter the data: ");
-    scanf("%d", &data);
-
-    stack[++top] = data;
-    printf("%d pushed to stack\n", data);
+else if(opt==3){
+ printf("Top is %d\n",stack[top]);
 }
-
-// Pop operation
-void pop() {
-    if (isEmpty()) {
-        printf("Stack is empty\n");
-        return;
-    }
-
-    printf("%d is deleted\n", stack[top]);
-    top--;
+else if(opt==4){
+printf("Stack:\n");
+for(i=0;i<=top;i++) printf("%d\n",stack[i]);
 }
-
-// Peek operation
-void peek() {
-    if (isEmpty()) {
-        printf("Stack is empty\n");
-        return;
-    }
-
-    printf("Top element is %d\n", stack[top]);
+else if(opt==5){
+exit(0);
 }
-
-// Display stack
-void display() {
-    if (isEmpty()) {
-        printf("Stack is empty\n");
-        return;
-    }
-
-    printf("Stack elements:\n");
-    for (int i = 0; i <= top; i++) {
-        printf("stack[%d] = %d\n", i + 1, stack[i]);
-    }
+else printf("Invalid option\n");
 }
-
-int main() {
-    int opt = 1;
-
-    while (1) {
-        printf("\n1) PUSH\n");
-        printf("2) POP\n");
-        printf("3) PEEK\n");
-        printf("4) DISPLAY\n");
-        printf("5) QUIT\n");
-        printf("Choose your option: ");
-        scanf("%d", &opt);
-
-        switch (opt) {
-            case 1: push(); break;
-            case 2: pop(); break;
-            case 3: peek(); break;
-            case 4: display(); break;
-            case 5: exit(0);
-            default: printf("Invalid option\n");
-        }
-    }
-
-    return 0;
+return 0;
 }
